47178ff6c19542ba6de35b156b95b1d2e978a1ee
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-05 13:16:22 -0300

    add products to carrito

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductosService } from '../productos/productos.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto',
@@ -10,9 +11,12 @@ import { ProductosService } from '../productos/productos.service';
 export class ProductoComponent {
   @Input() producto: any;
 
-  public productoId: any
-  public loading = false
-  constructor(private route: ActivatedRoute, private productosService: ProductosService) { }
+  public productoId: any;
+  public loading = false;
+  public carrito: any[] = [];
+  public carritoAgregado = false;
+
+  constructor(private route: ActivatedRoute, private productosService: ProductosService, private router: Router) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
@@ -20,18 +24,51 @@ export class ProductoComponent {
       if (productId) {
         this.getProductById(productId);
       }
-    })
+    });
+    this.filterLocalStorage();
   }
 
   getProductById(id: string) {
     this.productosService.getProductoById(id)
       .then(producto => {
         console.log(producto);
-        this.productoId = producto
-        this.loading = true
+        this.productoId = producto;
+        this.loading = true;
+        this.checkCarrito();
       })
       .catch(error => {
         console.error(error);
       });
+  }
+
+  agregarAlCarrito() {
+    const carritoActual = localStorage.getItem('carrito');
+    if (carritoActual) {
+      this.carrito = JSON.parse(carritoActual);
+    }
+    const existeProducto = this.carrito.find(producto => producto.id === this.productoId.id);
+    if (!existeProducto) {
+      this.carrito.push(this.productoId);
+      localStorage.setItem('carrito', JSON.stringify(this.carrito));
+      this.carritoAgregado = true;
+    }
+  }
+
+  filterLocalStorage() {
+    const carritoActual = localStorage.getItem('carrito');
+    if (carritoActual) {
+      this.carrito = JSON.parse(carritoActual);
+    }
+    this.checkCarrito();
+  }
+
+  checkCarrito() {
+    const existeProducto = this.carrito.find(producto => producto.id === this.productoId.id);
+    if (existeProducto) {
+      this.carritoAgregado = true;
+    }
+  }
+  goCarrito() {
+    this.router.navigate(['/carrito']);
   }
 }
