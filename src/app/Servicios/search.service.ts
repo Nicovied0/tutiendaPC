9060ca149ca7dc0a1cf001c677e936cf79019608
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-06 23:11:58 -0300

    service working and search bar complete

--- src/app/Servicios/search.service.ts
+++ src/app/Servicios/search.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { ProductosService } from './productos.service';
 
 @Injectable({ providedIn: 'root' })
 export class SearchService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private productosService: ProductosService) { }
 
-  soyConsole(){
-    console.log('aasdsada')
+  async getProductsByName(data: string) {
+    try {
+      const results = await this.productosService.getProductos();
+      const filteredProducts = results.filter((producto: any) => {
+        const nombre = producto.nombre.toLowerCase();
+        const categoria = producto.categoria.toLowerCase();
+        const termino = data.toLowerCase();
+        return nombre.includes(termino) || categoria.includes(termino);
+      });
+      console.log(filteredProducts, "resultados de búsqueda");
+      return filteredProducts;
+    } catch (error) {
+      console.error('Error occurred while getting products:', error);
+      throw error; // rethrow the error to be handled by the caller
+    }
   }
 }
--- src/app/serch-bar/serch-bar.component.ts
+++ src/app/serch-bar/serch-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SearchService } from '../Servicios/search.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-serch-bar',
@@ -7,9 +8,23 @@ import { SearchService } from '../Servicios/search.service';
   styleUrls: ['./serch-bar.component.css']
 })
 export class SerchBarComponent {
+  public value: any;
+  public showDropdown: boolean = false;
+  public resultados: any[] = [];
 
-  constructor(private search: SearchService) { }
-  console() {
-    this.search.soyConsole()
+  constructor(private search: SearchService, private router: Router) { }
+
+  searchButton(data: string) {
+    this.search.getProductsByName(data).then((resultados) => {
+      this.resultados = resultados;
+      this.showDropdown = true;
+    });
+  }
+  goProduct(id: number) {
+    console.log(id)
+    this.showDropdown = false
+    this.value = ''
+    this.router.navigate(['/producto', id]);
+
   }
 }
