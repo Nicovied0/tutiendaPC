de62259416118308426fd95c2f6491890eda49a1
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-14 13:21:01 -0300

    new icons in add products

--- src/app/add-products/add-products.component.ts
+++ src/app/add-products/add-products.component.ts
@@ -7,10 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AddProductsComponent {
   producto: any = {};
+  public imagenCargada = true
+
   constructor() {
     this.producto.dispible = true; // Asignar el valor por defecto a dispible
   }
 
+
+  cargarImagen() {
+    console.log(this.producto.imagen)
+    this.imagenCargada = false
+  }
+  editImagen() {
+    console.log(this.producto.imagen)
+    this.imagenCargada = true
+  }
 
   guardarProducto() {
     // Aquí puedes utilizar el objeto `producto` para guardar los valores en tu lógica
