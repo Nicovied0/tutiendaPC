772ad3cba61052d70733ed2283acc778af62e9e5
Nicovied0 <briannicolasoviedo@hotmail.com>
2023-06-14 01:21:05 -0300

    inicializate add new products, Lack of logic and the publication of the new product but the structure is ready

--- src/app/add-products/add-products.component.ts
+++ src/app/add-products/add-products.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-products.component.css']
 })
 export class AddProductsComponent {
+  producto: any = {};
+  constructor() {
+    this.producto.dispible = true; // Asignar el valor por defecto a dispible
+  }
+
+
+  guardarProducto() {
+    // Aquí puedes utilizar el objeto `producto` para guardar los valores en tu lógica
+    console.log(this.producto);
+  }
 
 }
