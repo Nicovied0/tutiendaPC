3ae678245dcad4eac5cea913e81a94a68161d29d
Nicovied0 <briannoviedo@gmail.com>
2023-11-30 17:58:49 -0300

    comment workout notification

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,33 +30,33 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     // Obtener la fecha de la última visita almacenada en localStorage
-    const lastVisitString = localStorage.getItem('lastVisit');
-    if (lastVisitString) {
-      this.lastVisit = new Date(lastVisitString);
-    }
+  //   const lastVisitString = localStorage.getItem('lastVisit');
+  //   if (lastVisitString) {
+  //     this.lastVisit = new Date(lastVisitString);
+  //   }
 
-    // Obtener la fecha actual
-    const currentDate = new Date();
+  //   // Obtener la fecha actual
+  //   const currentDate = new Date();
 
-    // Calcular la diferencia en milisegundos entre la fecha actual y la última visita
-    const diffMilliseconds = currentDate.getTime() - (this.lastVisit?.getTime() || 0);
+  //   // Calcular la diferencia en milisegundos entre la fecha actual y la última visita
+  //   const diffMilliseconds = currentDate.getTime() - (this.lastVisit?.getTime() || 0);
 
-    // Convertir la diferencia en horas
-    const diffHours = diffMilliseconds / (1000 * 60 * 60);
+  //   // Convertir la diferencia en horas
+  //   const diffHours = diffMilliseconds / (1000 * 60 * 60);
 
-    // Mostrar el mensaje solo si han pasado más de 24 horas desde la última visita
-    if (diffHours >= 24) {
-      Swal.fire({
-        title: 'El desarrollador está trabajando en nuevos cambios en la web!',
-        text: 'Pueden surgir errores!',
-        imageUrl: this.imagen,
-        confirmButtonText: 'Continuar',
-        imageHeight: 150
-      });
+  //   // Mostrar el mensaje solo si han pasado más de 24 horas desde la última visita
+  //   if (diffHours >= 24) {
+  //     Swal.fire({
+  //       title: 'El desarrollador está trabajando en nuevos cambios en la web!',
+  //       text: 'Pueden surgir errores!',
+  //       imageUrl: this.imagen,
+  //       confirmButtonText: 'Continuar',
+  //       imageHeight: 150
+  //     });
 
-      // Actualizar la fecha de la última visita en localStorage
-      localStorage.setItem('lastVisit', currentDate.toISOString());
-    }
-  }
+  //     // Actualizar la fecha de la última visita en localStorage
+  //     localStorage.setItem('lastVisit', currentDate.toISOString());
+  //   }
+  // }
 }
 
