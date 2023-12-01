cfa8d979eb86a89cfccac1d9b7084b7c7ae10396
Nicovied0 <briannoviedo@gmail.com>
2023-12-01 16:02:56 -0300

    add more stles

--- src/app/sponsor-scroll/sponsor-scroll.component.ts
+++ src/app/sponsor-scroll/sponsor-scroll.component.ts
@@ -5,15 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sponsor-scroll.component.html',
   styleUrls: ['./sponsor-scroll.component.css']
 })
-export class SponsorScrollComponent implements OnInit{
+export class SponsorScrollComponent implements OnInit {
   images: any[] = [];
+  images2: any[] = []
 
-  constructor(private imageService: ImageService) {}
+  constructor(private imageService: ImageService) { }
 
   ngOnInit(): void {
     this.imageService.getImages().subscribe((data) => {
       this.images = data;
-      console.log(data,"soy imagenes")
+      this.images2 = [...this.images];
+      this.images.pop();
+      console.log(data, "soy imagenes")
     });
+
   }
 }
